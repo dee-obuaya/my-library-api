58a860bca2ed256bde3e02b92fc0c3c20bfadb92
Chukwudumebi Obuaya <vannyglam@gmail.com>
2024-01-18 00:44:26 +0100

    update to schema for rating

--- models/book.js
+++ models/book.js
@@ -5,6 +5,11 @@ const BookSchema = new Schema({
     title: String,
     author: String,
     genre: [String],
+    rating: {
+        type: Number,
+        max: 5,
+        min: 1,
+    }
 });
 
 module.exports = mongoose.model('Book', BookSchema);
