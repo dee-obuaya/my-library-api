9d21a084cebf37b29a3dd5c6420bb214903ab467
Chukwudumebi Obuaya <vannyglam@gmail.com>
2024-01-19 00:56:34 +0100

    update minor change

--- middleware.js
+++ middleware.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const { bookSchema } = require('./schemas');
 const ExpressError = require('./utils/ExpressError');
 
