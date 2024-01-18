488e6f5e608fe629971dec7b9cb4a595ceb260c1
Chukwudumebi Obuaya <vannyglam@gmail.com>
2024-01-19 00:55:38 +0100

    update to include isAuthorized middleware to post, put and delete endpoints

--- routes/books.js
+++ routes/books.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const books = require('../controllers/books');
 const catchAsync = require('../utils/catchAsync');
-const { validateBook } = require('../middleware');
+const { isAuthorized, validateBook } = require('../middleware');
 
 
 router.route('/')
     .get(catchAsync(books.index))
-    .post(validateBook, catchAsync(books.addBook));
+    .post(isAuthorized, validateBook, catchAsync(books.addBook));
 
 router.route('/:id')
-    .put(validateBook, catchAsync(books.updateBook))
-    .delete(catchAsync(books.deleteBook));
+    .put(isAuthorized, validateBook, catchAsync(books.updateBook))
+    .delete(isAuthorized, catchAsync(books.deleteBook));
 
 router.get('/:title', catchAsync(books.getBook));
 
