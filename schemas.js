6be72b7d7e42f2d8ee1843ecd04b8d4b3d25ab2b
Chukwudumebi Obuaya <vannyglam@gmail.com>
2024-01-19 00:18:57 +0100

    update to include rating in validation

--- schemas.js
+++ schemas.js
@@ -4,6 +4,7 @@ module.exports.bookSchema = BaseJoi.object({
     book: BaseJoi.object({
         title: BaseJoi.string().required(),
         author: BaseJoi.string().required(),
-        genre: BaseJoi.array().required()
+        genre: BaseJoi.array().required(),
+        rating: BaseJoi.number().min(1).max(5).required(),
     }).required(),
 });
