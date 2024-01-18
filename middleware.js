bc8749da76f14d864b79042afc8c37974dee8c96
Chukwudumebi Obuaya <vannyglam@gmail.com>
2024-01-19 00:54:55 +0100

    update to add new middleware to check for authorization

--- middleware.js
+++ middleware.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const { bookSchema } = require('./schemas');
 const ExpressError = require('./utils/ExpressError');
+
 
 module.exports.validateBook = (req, res, next) => {
     const { error } = bookSchema.validate(req.body);
@@ -8,6 +10,21 @@ module.exports.validateBook = (req, res, next) => {
         res.json({
             err: new ExpressError(msg, 400)
         })
+    } else {
+        next();
+    }
+}
+
+module.exports.isAuthorized = (req, res, next) => {
+    const { authKey } = req.query;
+    if (!authKey) {
+        res.json({
+            err: new ExpressError('An authorization key is required. Please pass authKey as a query.', 400),
+        });
+    } else if (authKey !== process.env.AUTH_KEY) {
+        res.json({
+            err: new ExpressError('Wrong authorization key.', 400),
+        });
     } else {
         next();
     }
