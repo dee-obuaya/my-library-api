a75f586e3e9f015ae36a823c3c3abe18f048d8e9
Chukwudumebi Obuaya <vannyglam@gmail.com>
2024-01-19 21:40:18 +0100

    update to move db and starting server logic to server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const mongoose = require('mongoose');
+const app = require('./app');
+const dbUrl = process.env.MONGO_URL;
+const PORT = process.env.PORT || 3000;
+
+/* Connecting to the database and then starting the server. */
+main()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Serving on port ${PORT}`);
+        });
+    })
+    .catch(err => console.error.bind(console, 'connection error:'));
+async function main() {
+    await mongoose.connect(dbUrl);
+}
