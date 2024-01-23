10e112311cd7e4884c7f2f9b8bff66033cae6a65
Chukwudumebi Obuaya <vannyglam@gmail.com>
2024-01-23 16:01:13 +0100

    update complete pagination for getBooksInGenre and getBooksByAuthor controllers

--- controllers/books.js
+++ controllers/books.js
@@ -1,8 +1,10 @@
 const Book = require('../models/book');
 const ExpressError = require('../utils/ExpressError');
 
+const pageLimit = 8;
+
 module.exports.getAllBooks = async (req, res) => {
-    const pageLimit = 8;
+    // const pageLimit = 8;
     let { rating, page = 1 } = req.query;
     if (isNaN(page) || page < 1) {
         page = 1;
@@ -36,7 +38,7 @@ module.exports.getAllBooks = async (req, res) => {
         result = result[0];
         const pageInfo = { ...result.metaData[0], booksOnCurrentPage: result.data.length };
 
-        res.status(200).json({
+        return res.status(200).json({
             message: 'success',
             pageInfo: pageInfo,
             books: result.data,
@@ -116,32 +118,93 @@ module.exports.getBookByTitle = async (req, res) => {
 
 module.exports.getBooksInGenre = async (req, res) => {
     const genre = req.params.genre;
-    const { rating } = req.query;
-    if (rating) {
+    let { rating, page = 1 } = req.query;
+    if (isNaN(page) || page < 1) {
+        page = 1;
+    }
+    const skip = (page - 1) * pageLimit;
+
+    if (rating && !isNaN(rating) && !(rating > 5) && !(rating < 1)) {
         const ratingVal = parseInt(rating);
-        const booksRatedInGenre = await Book.find({
-            genre: { $in: [genre] },
-            rating: { $gte: ratingVal, $lt: ratingVal + 1 }
-        });
-        if (booksRatedInGenre.length > 0) {
+        let result = await Book.aggregate([
+            {
+                $match: {
+                    genre: { $regex: genre, $options: 'i' },
+                    rating: { $gte: ratingVal, $lt: ratingVal + 1 }
+                }
+            },
+            {
+                $facet: {
+                    metaData: [
+                        { $count: 'totalBooks' },
+                        {
+                            $addFields: {
+                                currentPage: page,
+                                totalPages: { $ceil: { $divide: ['$totalBooks', pageLimit] } },
+                            }
+                        }
+                    ],
+                    data: [
+                        { $sort: { title: 1 } },
+                        { $skip: skip },
+                        { $limit: pageLimit }
+                    ],
+                },
+            },
+        ]);
+
+        result = result[0];
+        const pageInfo = { ...result.metaData[0], booksOnCurrentPage: result.data.length };
+
+        if (result.data.length > 0) {
             return res.status(200).json({
                 message: 'success',
-                totalBooks: booksRatedInGenre.length,
-                books: booksRatedInGenre,
+                pageInfo: pageInfo,
+                books: result.data,
             });
         } else {
-            const err = new ExpressError(`Sorry we could not find any books rated in the range of ${ratingVal} in ${genre}`, 404);
+            const err = new ExpressError(`Sorry we could not find any books in ${genre} with a rating in the range of ${rating}`, 404);
             return res.status(err.statusCode).json({ err });
         }
     }
-    const booksInGenre = await Book.find({
-        genre: { $in: [genre] }
-    });
-    if (booksInGenre.length > 0) {
+
+    let result = await Book.aggregate([
+        {
+            $match: {
+                genre: { $regex: genre, $options: 'i' },
+            }
+        },
+        {
+            $facet: {
+                metaData: [
+                    { $count: 'totalBooks' },
+                    {
+                        $addFields: {
+                            currentPage: page,
+                            totalPages: { $ceil: { $divide: ['$totalBooks', pageLimit] } },
+                        }
+                    }
+                ],
+                data: [
+                    { $sort: { title: 1 } },
+                    { $skip: skip },
+                    { $limit: pageLimit }
+                ],
+            },
+        },
+    ]);
+
+    result = result[0];
+    const pageInfo = {
+        ...result.metaData[0],
+        booksOnCurrentPage: result.data.length
+    };
+
+    if (result.data.length > 0) {
         return res.status(200).json({
             message: 'success',
-            totalBooks: booksInGenre.length,
-            books: booksInGenre,
+            pageInfo: pageInfo,
+            books: result.data,
         });
     } else {
         const err = new ExpressError(`Sorry we could not find any books in ${genre}`, 404);
@@ -151,32 +214,93 @@ module.exports.getBooksInGenre = async (req, res) => {
 
 module.exports.getBooksByAuthor = async (req, res) => {
     const author = req.params.author;
-    const { rating } = req.query;
-    if (rating) {
+    let { rating, page = 1 } = req.query;
+    if (isNaN(page) || page < 1) {
+        page = 1;
+    }
+    const skip = (page - 1) * pageLimit;
+
+    if (rating && !isNaN(rating) && !(rating > 5) && !(rating < 1)) {
         const ratingVal = parseInt(rating);
-        const booksRatedByAuthor = await Book.find({
-            author: author,
-            rating: { $gte: ratingVal, $lt: ratingVal + 1 }
-        });
-        if (booksRatedByAuthor.length > 0) {
+        let result = await Book.aggregate([
+            {
+                $match: {
+                    author: { $regex: author, $options: 'i' },
+                    rating: { $gte: ratingVal, $lt: ratingVal + 1 }
+                }
+            },
+            {
+                $facet: {
+                    metaData: [
+                        { $count: 'totalBooks' },
+                        {
+                            $addFields: {
+                                currentPage: page,
+                                totalPages: { $ceil: { $divide: ['$totalBooks', pageLimit] } },
+                            }
+                        }
+                    ],
+                    data: [
+                        { $sort: { title: 1 } },
+                        { $skip: skip },
+                        { $limit: pageLimit }
+                    ],
+                },
+            },
+        ]);
+
+        result = result[0];
+        const pageInfo = { ...result.metaData[0], booksOnCurrentPage: result.data.length };
+
+        if (result.data.length > 0) {
             return res.status(200).json({
                 message: 'success',
-                totalBooks: booksRatedByAuthor.length,
-                books: booksRatedByAuthor,
+                pageInfo: pageInfo,
+                books: result.data,
             });
         } else {
-            const err = new ExpressError(`Sorry we could not find any books rated in the range of ${ratingVal} by ${author}`, 404);
+            const err = new ExpressError(`Sorry we could not find any books rated by ${author} in the range of ${ratingVal}`, 404);
             return res.status(err.statusCode).json({ err });
         }
     }
-    const booksByAuthor = await Book.find({
-        author: author
-    });
-    if (booksByAuthor.length > 0) {
+
+    let result = await Book.aggregate([
+        {
+            $match: {
+                author: { $regex: author, $options: 'i' },
+            }
+        },
+        {
+            $facet: {
+                metaData: [
+                    { $count: 'totalBooks' },
+                    {
+                        $addFields: {
+                            currentPage: page,
+                            totalPages: { $ceil: { $divide: ['$totalBooks', pageLimit] } },
+                        }
+                    }
+                ],
+                data: [
+                    { $sort: { title: 1 } },
+                    { $skip: skip },
+                    { $limit: pageLimit }
+                ],
+            },
+        },
+    ]);
+
+    result = result[0];
+    const pageInfo = {
+        ...result.metaData[0],
+        booksOnCurrentPage: result.data.length
+    };
+
+    if (result.data.length > 0) {
         return res.status(200).json({
             message: 'success',
-            totalBooks: booksByAuthor.length,
-            books: booksByAuthor,
+            pageInfo: pageInfo,
+            books: result.data,
         });
     } else {
         const err = new ExpressError(`Sorry we could not find any books by ${author}`, 404);
